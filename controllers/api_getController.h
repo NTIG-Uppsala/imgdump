386d44d23dbf2b2eae7b486a37e465c052b1f144
liljekvist <mikael0225@outlook.com>
2021-12-02 11:05:25 +0100

    forgot to rename function in headerfile

--- controllers/api_getController.h
+++ controllers/api_getController.h
@@ -12,13 +12,13 @@ class getController:public drogon::HttpController<getController>
     //METHOD_ADD(getController::your_method_name,"/{1}/{2}/list",Get);//path is /api/getController/{arg1}/{arg2}/list
     //ADD_METHOD_TO(getController::your_method_name,"/absolute/path/{1}/{2}/list",Get);//path is /absolute/path/{arg1}/{arg2}/list
     ADD_METHOD_TO(getController::ViewTestFileUpload,"/test/sendfile",Get);//path is /test/sendfile
-    ADD_METHOD_TO(getController::GetImageFromDatabase,"/image/{1}",Get);//path is /image/{1}
+    ADD_METHOD_TO(getController::GetImageAndRespondToClient,"/image/{1}",Get);//path is /image/{1}
 
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,int p1,std::string p2);
     // void your_method_name(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,double p1,int p2) const;
     void ViewTestFileUpload(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback);
-    void GetImageFromDatabase(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback, std::string imageId);
+    void GetImageAndRespondToClient(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback, std::string imageId);
 };
 }
