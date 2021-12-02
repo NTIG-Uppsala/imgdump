d5e7010841cc87652618fa4f9ddba731ae636a3b
liljekvist <mikael0225@outlook.com>
2021-12-02 15:32:17 +0100

    Reverted commit issue

--- controllers/api_postController.cc
+++ controllers/api_postController.cc
@@ -3,8 +3,8 @@ using namespace api;
 //add definition of your processing function here
 void postController::uploadEndpoint(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback){
     Json::Value ret;
-    MultiPartParser fileUpload; // File Parser
-    if (fileUpload.parse(req) != 0 || fileUpload.getFiles().size() != 1) // Är mer eller mindre än 1 fil eller storleken på filen är 0b;
+    MultiPartParser fileUpload;
+    if (fileUpload.parse(req) != 0 || fileUpload.getFiles().size() != 1)
     {
         ret["status"] = 403;
         ret["message"] = "Must be one file";
@@ -14,8 +14,8 @@ void postController::uploadEndpoint(const HttpRequestPtr& req,std::function<void
     }
 
     auto &file = fileUpload.getFiles()[0];
-    
-    if(file.getFileExtension() == "gif"){ // Filen är en gif
+    auto fileExt = file.getFileExtension();
+    if(fileExt == "gif"){
         ret["status"] = 403;
         ret["message"] = "Gifs not allowed";
         auto resp=HttpResponse::newHttpJsonResponse(ret);
@@ -23,7 +23,7 @@ void postController::uploadEndpoint(const HttpRequestPtr& req,std::function<void
         return;
     }
 
-    if(file.getFileType() != FileType::FT_IMAGE){ // Filen är inte en bild
+    if(file.getFileType() != FileType::FT_IMAGE){
         ret["status"] = 403;
         ret["message"] = "Must be a image";
         auto resp=HttpResponse::newHttpJsonResponse(ret);
@@ -33,11 +33,25 @@ void postController::uploadEndpoint(const HttpRequestPtr& req,std::function<void
 
     auto md5 = file.getMd5();
     auto fileUuid = drogon::utils::getUuid();
+    file.saveAs(fileUuid);
+    std::ostringstream databaseQuery;
+    databaseQuery << "INSERT INTO `images`(`id`, `uuid`, `fileExt`, `md5`) VALUES (NULL,'" << fileUuid << "','" << fileExt << "','" << md5 << "')";
+
+    auto clientPtr = drogon::app().getDbClient();
+    auto sentQuery = clientPtr -> execSqlAsyncFuture(databaseQuery.str(), "default");
+    try {
+      auto result = sentQuery.get();
+    } 
+      catch (int error) {
+        std::cerr << "errors:" << error << std::endl;
+    }
+    
+
     ret["status"] = 200;
     ret["md5"] = md5;
-    ret["filename"] = fileUuid;
     ret["message"] = "Successful upload";
-    file.saveAs(fileUuid);
+    ret["id"] = fileUuid;
     auto resp=HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
+
 }
