beb32e9fc52582022175c2703f17c47c6f931010
Sebastian Clavensjo <Sebastian.clavensjo@elev.ga.ntig.se>
2021-12-16 11:45:32 +0100

    Delete function now deletes the row from the database
    Co-authored-by: PontusForsmark <pontus.forsmark@elev.ga.ntig.se>

--- controllers/api_postController.cc
+++ controllers/api_postController.cc
@@ -56,17 +56,22 @@ void postController::uploadEndpoint(const HttpRequestPtr& req,std::function<void
     callback(resp);
 }
 
-
+// This function deletes an image from database and storage, using uuid.
 void postController::deleteEndpoint(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback, std::string imageId){
     Json::Value ret;
-    char acceptedChars[] = "1234567890ABCDEF";
+    std::ostringstream databaseQuery1;
+    std::ostringstream databaseQuery2;
     bool validImageId = true;
+    std::string exists;
+    // uuid are always 32 characters long in C++
     if (imageId.length() != 32){
         validImageId = false;
     }
-    for(auto & elem: imageId){
+    // Checks if uuid only contains valid characters
+    char acceptedChars[] = "1234567890ABCDEF";
+    for(auto& elem: imageId){
         bool validChar = false;
-        for(auto & character: acceptedChars) {
+        for(auto& character: acceptedChars) {
             if ( elem == character){
                 validChar = true;
             }
@@ -75,14 +80,43 @@ void postController::deleteEndpoint(const HttpRequestPtr& req,std::function<void
             validImageId = false;
         }
     }
-    if (validImageId == false){
-        ret["status"] = 403;
-        ret["message"] = "Invalid uuid =(";
+    if (validImageId == true){
+        auto clientPtr = drogon::app().getDbClient();
+        databaseQuery1 << "SELECT `uuid` FROM `images` WHERE uuid = '" << imageId << "';"; //"SELECT EXISTS (SELECT * FROM `images` WHERE uuid = '" << imageId << "');"; //Formaterar queryn
+        auto sentQuery1 = clientPtr -> execSqlAsyncFuture(databaseQuery1.str(), "default"); //Skickar en query till databasen
+        try {
+            auto result = sentQuery1.get(); //Hämtar resultat från query
+            for (auto row : result)
+            {
+                exists = row["uuid"].as<std::string>();
+            }
+        } 
+            catch (int error) {
+                std::cerr << "errors:" << error << std::endl;
+        }
+        if (exists == ""){
+            ret["status"] = 403;
+            ret["message"] = "uuid not found in database";
+            ret["valid image id checker"] = validImageId;
+            ret["image id length"] = imageId.length();
+            auto resp=HttpResponse::newHttpJsonResponse(ret);
+            imageId = "";
+            callback(resp);
+        }
+        databaseQuery2 << "DELETE FROM `images` WHERE uuid = '" << imageId << "';"; //Formaterar queryn
+        auto sentQuery2 = clientPtr -> execSqlAsyncFuture(databaseQuery2.str(), "default"); //Skickar en query till databasen
+        ret["status"] = 200;
+        ret["message"] = "Image successfully deleted";
+        ret["exists printout"] = exists;
         auto resp=HttpResponse::newHttpJsonResponse(ret);
+        imageId = "";
         callback(resp);
     }
-    ret["status"] = 200;
-    ret["message"] = "Valid uuid!";
-    auto resp=HttpResponse::newHttpJsonResponse(ret);
-    callback(resp);
+    else{
+        ret["status"] = 403;
+        ret["message"] = "Invalid uuid";
+        auto resp=HttpResponse::newHttpJsonResponse(ret);
+        imageId = "";
+        callback(resp);
+    }
 }
